93e82cac044f5a345b2595c7020c3d680ad7b633
Arkadiusz Rasiński <arkadiusz.rasinski.programista@gmail.com>
2021-06-06 14:12:05 +0200

    Added tables with incomes and expenses

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ function displayLoginPanel() {
 
 $('#login-option').on("click", displayLoginPanel);
 $('#registration-option').on("click", displayRegistrationPanel);
+
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+var maxDate = year + '-' + month + '-' + day;
+$('#dateID').attr('max', maxDate);
+$('#dateID').val(maxDate);
+
+var heightDivTableResults = parseFloat($('.div-table-results:last-child').css('height'));
+$('.article-results').css('height', heightDivTableResults);
+$('.div-table-results:first-child').css('height', $('.div-table-results:last-child').css('height'));
